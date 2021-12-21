62e6cb04c9f68b71489d4796420e71a0a21c23c6
Rark7040 <crimera.hello@gmail.com>
2021-12-21 11:48:11 +0900

    コマンドのひな形追加

---
+++ SimpleFill/src/rark/simple_fill/command/SimpleFillCommand.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace rark\simple_fill\command;
+
+use rark\simple_fill\libs\cortexpe\commando\BaseCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use rark\simple_fill\libs\cortexpe\commando\args\BooleanArgument;
+
+class SimpleFillCommand extends BaseCommand{
+
+	public function __construct(PluginBase $owner){
+		parent::__construct(
+			$owner,
+			'simplefill',
+			'Simple Fill',
+			['sf']
+		);
+	}
+
+	protected function prepare():void{
+		$this->setPermission('simple_fill.command.op');
+		$this->registerArgument(0, new BooleanArgument('mode', true));
+	}
+
+	public function onRun(CommandSender $sender, string $command, array $args):void{
+		if(!$sender instanceof Player){
+			$sender->sendMessage('§cゲーム内で実行してください');
+			return;
+		}
+
+	}
+}
